e971735dcf5e1bb2883954c93f3a3bbe99484eb9
viseth89 <viseth89@gmail.com>
2021-01-17 08:28:00 -0600

    Day One refresher

---
+++ day_one.py
@@ -0,0 +1,17 @@
+print('Hello welcome to Day One')
+
+print("This is a great refresher")
+city = input('Please tell us what city you are from: \n')
+pet = input('Please tell us the name of your pet: \n ')
+print ('The name of your band should be ' + city + ' ' + pet )
+
+###
+# Lesson 1 - 
+# print()
+# This is basic how to print to line command for Python
+
+# Lesson 2 - 
+# Concatenate with +
+
+# Lesson 3 - 
+# Input - Allows us to receive an input -> store it to variable 
